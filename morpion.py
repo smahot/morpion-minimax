6ceecdfcb4bce80c6ba49db7972ff9ca712b6bd4
smahot <steve.mahot@edu.devinci.fr>
2019-04-02 15:01:43 +0200

    ajout fonction estTerminal

--- morpion.py
+++ morpion.py
@@ -3,4 +3,34 @@ class morpion:
     def __init__(self):
         self.grille = [[' ' , ' ' , ' '],[' ' , ' ' , ' '],[' ' , ' ' , ' ']]
     
+    def estTerminal(self):
+        res = False
+        #3 lignes :
+        for i in range (3):
+            if res == False:
+                break
+            else :
+                res = self.identiqueList(self.grille[i])
+
+        #3 colonnes :
+        for i in range (3):
+            if res == False:
+                break
+            else :
+                res = self.identiqueList(self.grille[j][i] for j in range(3))
+
+        #2 diagonales :
+        if res != False:
+            res = self.identiqueList(self.grille[i][i] for i in range(3))
+        if res != False:
+            res = self.identiqueList(self.grille[i][2-i] for i in range(3))
+        return res
+
+        
+    def identiqueList(self, liste):
+        for i in liste:
+            res = liste[0]
+            if i != liste[0]:
+                res = False
+        return res
 

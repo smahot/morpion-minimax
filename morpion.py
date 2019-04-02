976ddd0f107c1b961da39d001448352e453eeb41
MatthieuLouf <m.louf@orange.fr>
2019-04-02 14:21:06 +0200

    grille en hard

--- morpion.py
+++ morpion.py
@@ -1,7 +1,6 @@
 class morpion:
 
     def __init__(self):
-        self.grille = list()
-        grille.append([' ',' ',' ']) for i in range(0,3)
-        
+        self.grille = [[' ' , ' ' , ' '],[' ' , ' ' , ' '],[' ' , ' ' , ' ']]
+    
 
